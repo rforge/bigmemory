182b66e9ddde0b6355d5614ad96e7282070fb5ae
kaneplusplus <kaneplusplus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-04 20:25:35 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bigmemory@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bigmemory/include/SharedCounter.h
+++ pkg/bigmemory/include/SharedCounter.h
@@ -1,26 +1,3 @@
-/*
- *  bigmemory: an R package for managing massive matrices using C,
- *  with support for shared memory.
- *
- *  Copyright (C) 2009 John W. Emerson and Michael J. Kane
- *
- *  This file is part of bigmemory.
- *
- *  bigmemory is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published
- *  by the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, a copy is available at
- *  http://www.r-project.org/Licenses/
- */
-
 #ifndef _SHARED_COUNTER_H
 #define _SHARED_COUNTER_H
 
@@ -36,7 +13,7 @@ class SharedCounter
     SharedCounter(): _pVal(NULL),_pRegion(NULL){};
     ~SharedCounter(){reset();};
 
-    bool init( const std::string resourceName);
+    bool init( const std::string &resourceName);
   public:
     index_type get() const;
     bool reset();
