80822a7c8bb9ee3737598e79873b91e1b261ec8d
kaneplusplus <kaneplusplus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-23 04:39:44 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bigmemory@76 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/synchronicity/R/mutexes.R
+++ pkg/synchronicity/R/mutexes.R
@@ -105,38 +105,9 @@ boost.mutex=function(sharedName=NULL, timeout=NULL)
 
 
 setClass('descriptor', representation(description='list'))
-setGeneric('describe', function(x) standardGeneric('describe'))
 setGeneric('description', function(x) standardGeneric('description'))
 setMethod('description', signature(x='descriptor'),
   function(x) return(x@description))
 
 setClass('boost.mutex.descriptor', contains='descriptor')
 
-setMethod('describe', signature(x='boost.mutex'),
-  function(x)
-  {
-    return(new('boost.mutex.descriptor', 
-      description=list(shared.name=shared.name(x), timeout=timeout(x))))
-  })
-
-setGeneric('attach.resource', function(obj, ...) 
-  standardGeneric('attach.resource'))
-
-setMethod('attach.resource', signature(obj='character'),
-  function(obj, ...)
-  {
-    path = match.call()[['path']]
-    if (is.null(path))
-    {
-      path = ''
-    }
-    info <- dget(paste(fix_path(path), obj, sep=""))
-    return(attach.resource(info))
-  })
-
-setMethod('attach.resource', signature(obj='boost.mutex.descriptor'),
-  function(obj, ...)
-  {
-    desc = description(obj)
-    return(boost.mutex(sharedName = desc$shared.name, timeout = desc$timeout))
-  })
--- pkg/synchronicity/R/zzz.R
+++ pkg/synchronicity/R/zzz.R
@@ -1,5 +1,40 @@
 .onLoad <- function(libname, pkgname)
 {
+  if (!isGeneric('describe'))
+    setGeneric('describe', function(x) standardGeneric('describe'))
+  
+  setMethod('describe', signature(x='boost.mutex'),
+    function(x)
+    {
+      return(new('boost.mutex.descriptor', 
+        description=list(shared.name=shared.name(x), 
+        timeout=timeout(x))))
+    })
+
+  if (!isGeneric('attach.resource'))
+  {
+    setGeneric('attach.resource', function(obj, ...) 
+      standardGeneric('attach.resource'))
+
+    setMethod('attach.resource', signature(obj='character'),
+      function(obj, ...)
+      {
+        path = match.call()[['path']]
+        if (is.null(path))
+        {
+          path = ''
+        }
+        info <- dget(paste(fix_path(path), obj, sep=""))
+        return(attach.resource(info))
+      })
+  }
+  setMethod('attach.resource', signature(obj='boost.mutex.descriptor'),
+    function(obj, ...)
+    {
+      desc = description(obj)
+      return(boost.mutex(sharedName = desc$shared.name, 
+        timeout = desc$timeout))
+    })
   library.dynam('synchronicity', pkgname, libname);
 }
 
