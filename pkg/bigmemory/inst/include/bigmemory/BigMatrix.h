8b9b9119eb189dec2275278173943711b4b72430
kaneplusplus <kaneplusplus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-21 17:56:27 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bigmemory@233 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bigmemory/inst/include/bigmemory/BigMatrix.h
+++ pkg/bigmemory/inst/include/bigmemory/BigMatrix.h
@@ -82,7 +82,8 @@ class BigMatrix : public boost::noncopyable
     bool column_names( const Names &newColNames )
     {
       if ( !is_submatrix() && 
-        (newColNames.size() == static_cast<Names::size_type>(ncol())) )
+        (newColNames.size() == static_cast<Names::size_type>(ncol())) ||
+        newColNames.size() == 0 )
       {
         _colNames = newColNames; 
         return true;
@@ -100,7 +101,8 @@ class BigMatrix : public boost::noncopyable
     bool row_names( const Names &newRowNames )
     {
       if ( !is_submatrix() && 
-        (newRowNames.size() == static_cast<Names::size_type>(nrow())) )
+        (newRowNames.size() == static_cast<Names::size_type>(nrow())) ||
+        newRowNames.size() == 0 )
       {
         _rowNames = newRowNames; 
         return true;
