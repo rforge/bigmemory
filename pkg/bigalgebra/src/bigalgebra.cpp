d0d56c4f9a58bcdc4bd565baf78d7b7443807fc2
blewis <blewis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-05 00:02:38 +0000

    Working compilation with reference BLAS, ATLAS, MKL and ACML. Now on to Jay's index value issue and then to Windows
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bigmemory@279 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bigalgebra/src/bigalgebra.cpp
+++ pkg/bigalgebra/src/bigalgebra.cpp
@@ -9,9 +9,10 @@ extern "C"
 {
 // The R/C interface functions.
 
-  void dgemm_wrapper (SEXP TRANSA, SEXP TRANSB, SEXP M, SEXP N, SEXP K, SEXP ALPHA,
-	      SEXP A, SEXP LDA, SEXP B, SEXP LDB, SEXP BETA, SEXP C, SEXP LDC,
-	      SEXP A_isBM, SEXP B_isBM, SEXP C_isBM, SEXP C_offset)
+  void dgemm_wrapper (SEXP TRANSA, SEXP TRANSB, SEXP M, SEXP N, SEXP K,
+                      SEXP ALPHA, SEXP A, SEXP LDA, SEXP B, SEXP LDB,
+                      SEXP BETA, SEXP C, SEXP LDC, SEXP A_isBM, SEXP B_isBM,
+                      SEXP C_isBM, SEXP C_offset)
   {
 /*XXX j should be pointer-sized */
     int j = *(INTEGER_DATA (C_offset));
@@ -19,68 +20,120 @@ extern "C"
     double *pB = make_double_ptr (B, B_isBM);
     double *pC = make_double_ptr (C, C_isBM) + j;
 #ifdef INT64
-    long MM = (long)*(DOUBLE_DATA (M));
-    long NN = (long)*(DOUBLE_DATA (N));
-    long KK = (long)*(DOUBLE_DATA (K));
-    long LDAA = (long)*(DOUBLE_DATA (LDA));
-    long LDBB = (long)*(DOUBLE_DATA (LDB));
-    long LDCC = (long)*(DOUBLE_DATA (LDC));
+    long MM = (long) *(DOUBLE_DATA (M));
+    long NN = (long) *(DOUBLE_DATA (N));
+    long KK = (long) *(DOUBLE_DATA (K));
+    long LDAA = (long) *(DOUBLE_DATA (LDA));
+    long LDBB = (long) *(DOUBLE_DATA (LDB));
+    long LDCC = (long) *(DOUBLE_DATA (LDC));
 #else
-    int MM = (int)*(DOUBLE_DATA (M));
-    int NN = (int)*(DOUBLE_DATA (N));
-    int KK = (int)*(DOUBLE_DATA (K));
-    int LDAA = (int)*(DOUBLE_DATA (LDA));
-    int LDBB = (int)*(DOUBLE_DATA (LDB));
-    int LDCC = (int)*(DOUBLE_DATA (LDC));
+    int MM = (int) *(INTEGER_DATA (M));
+    int NN = (int) *(INTEGER_DATA (N));
+    int KK = (int) *(INTEGER_DATA (K));
+    int LDAA = (int) *(INTEGER_DATA (LDA));
+    int LDBB = (int) *(INTEGER_DATA (LDB));
+    int LDCC = (int) *(INTEGER_DATA (LDC));
 #endif
-      dgemm_ ((char *)CHARACTER_VALUE (TRANSA), 
-              (char *)CHARACTER_VALUE (TRANSB),
-	      &MM, &NN, &KK, NUMERIC_DATA (ALPHA), pA, &LDAA, pB,
-	      &LDBB, NUMERIC_DATA (BETA), pC, &LDCC);
+#ifdef ACMLBLAS
+      dgemm_ ((char *) CHARACTER_VALUE (TRANSA),
+              (char *) CHARACTER_VALUE (TRANSB),
+              &MM, &NN, &KK, NUMERIC_DATA (ALPHA), pA, &LDAA, pB,
+              &LDBB, NUMERIC_DATA (BETA), pC, &LDCC, 1, 1);
+#else
+      dgemm_ ((char *) CHARACTER_VALUE (TRANSA),
+              (char *) CHARACTER_VALUE (TRANSB),
+              &MM, &NN, &KK, NUMERIC_DATA (ALPHA), pA, &LDAA, pB,
+              &LDBB, NUMERIC_DATA (BETA), pC, &LDCC);
+#endif
   }
-/*
-  void dcopy_wrapper (SEXP N, SEXP X, SEXP INCX, SEXP Y, SEXP INCY, SEXP X_isBM,
-	      SEXP Y_isBM)
+
+  void dcopy_wrapper (SEXP N, SEXP X, SEXP INCX, SEXP Y, SEXP INCY,
+                      SEXP X_isBM, SEXP Y_isBM)
   {
     double *pX = make_double_ptr (X, X_isBM);
     double *pY = make_double_ptr (Y, Y_isBM);
-    dcopy_ (INDEX (N), pX, INDEX (INCX), pY,
-	    INDEX (INCY));
+#ifdef INT64
+    long NN = (long) *(DOUBLE_DATA (N));
+    long INCXX = (long) *(DOUBLE_DATA (INCX));
+    long INCYY = (long) *(DOUBLE_DATA (INCY));
+#else
+    int NN = (int) *(INTEGER_DATA (N));
+    int INCXX = (long) *(INTEGER_DATA (INCX));
+    int INCYY = (long) *(INTEGER_DATA (INCY));
+#endif
+    dcopy_ (&NN, pX, &INCXX, pY, &INCYY);
   }
 
   void dscal_wrapper (SEXP N, SEXP ALPHA, SEXP Y, SEXP INCY, SEXP Y_isBM)
   {
     double *pY = make_double_ptr (Y, Y_isBM);
-    dscal_ (INDEX (N), NUMERIC_DATA (ALPHA), pY, INDEX (INCY));
+#ifdef INT64
+    long NN = (long) *(DOUBLE_DATA (N));
+    long INCYY = (long) *(DOUBLE_DATA (INCY));
+#else
+    int NN = (int) *(INTEGER_DATA (N));
+    int INCYY = (long) *(INTEGER_DATA (INCY));
+#endif
+    dscal_ (&NN, NUMERIC_DATA (ALPHA), pY, &INCYY);
   }
 
-  void daxpy_wrapper (SEXP N, SEXP ALPHA, SEXP X, SEXP INCX, SEXP Y, SEXP INCY,
-	      SEXP X_isBM, SEXP Y_isBM)
+  void daxpy_wrapper (SEXP N, SEXP ALPHA, SEXP X, SEXP INCX, SEXP Y,
+                      SEXP INCY, SEXP X_isBM, SEXP Y_isBM)
   {
     double *pX = make_double_ptr (X, X_isBM);
     double *pY = make_double_ptr (Y, Y_isBM);
-    daxpy_ (INDEX (N), NUMERIC_DATA (ALPHA), pX, INDEX (INCX),
-	    pY, INDEX (INCY));
+#ifdef INT64
+    long NN = (long) *(DOUBLE_DATA (N));
+    long INCXX = (long) *(DOUBLE_DATA (INCX));
+    long INCYY = (long) *(DOUBLE_DATA (INCY));
+#else
+    int NN = (int) *(INTEGER_DATA (N));
+    int INCXX = (long) *(INTEGER_DATA (INCX));
+    int INCYY = (long) *(INTEGER_DATA (INCY));
+#endif
+    daxpy_ (&NN, NUMERIC_DATA (ALPHA), pX, &INCXX, pY, &INCYY);
   }
 
   void dgeqrf_wrapper (SEXP M, SEXP N, SEXP A, SEXP LDA, SEXP TAU, SEXP WORK,
-	       SEXP LWORK, SEXP INFO, SEXP A_isBM, SEXP TAU_isBM,
-	       SEXP WORK_isBM)
+                       SEXP LWORK, SEXP INFO, SEXP A_isBM, SEXP TAU_isBM,
+                       SEXP WORK_isBM)
   {
     double *pA = make_double_ptr (A, A_isBM);
     double *pTAU = make_double_ptr (TAU, TAU_isBM);
     double *pWORK = make_double_ptr (WORK, WORK_isBM);
-    dgeqrf_ (INDEX (M), INDEX (N), pA, INDEX (LDA),
-	     pTAU, pWORK, INDEX (LWORK), INDEX (INFO));
+#ifdef INT64
+    long MM = (long) *(DOUBLE_DATA (M));
+    long NN = (long) *(DOUBLE_DATA (N));
+    long LDAA = (long) *(DOUBLE_DATA (LDA));
+    long LWORKK = (long) *(DOUBLE_DATA (LWORK));
+    long INFOO = (long) *(DOUBLE_DATA (INFO));
+#else
+    int MM = (int) *(INTEGER_DATA (M));
+    int NN = (int) *(INTEGER_DATA (N));
+    int LDAA = (int) *(INTEGER_DATA (LDA));
+    int LWORKK = (int) *(INTEGER_DATA (LWORK));
+    int INFOO = (int) *(INTEGER_DATA (INFO));
+#endif
+    dgeqrf_ (&MM, &NN, pA, &LDAA, pTAU, pWORK, &LWORKK, &INFOO);
   }
 
-  void dpotrf_wrapper (SEXP UPLO, SEXP N, SEXP A, SEXP LDA, SEXP INFO, SEXP A_isBM)
+  void dpotrf_wrapper (SEXP UPLO, SEXP N, SEXP A, SEXP LDA, SEXP INFO,
+                       SEXP A_isBM)
   {
     double *pA = make_double_ptr (A, A_isBM);
-    dpotrf_ (CHARACTER_VALUE (UPLO), INDEX (N), pA, INDEX (LDA),
-	     INDEX (INFO));
+#ifdef INT64
+    long NN = (long) *(DOUBLE_DATA (N));
+    long LDAA = (long) *(DOUBLE_DATA (LDA));
+    long INFOO = (long) *(DOUBLE_DATA (INFO));
+#else
+    int NN = (int) *(INTEGER_DATA (N));
+    int LDAA = (int) *(INTEGER_DATA (LDA));
+    int INFOO = (int) *(INTEGER_DATA (INFO));
+#endif
+    dpotrf_ (CHARACTER_VALUE (UPLO), &NN, pA, &LDAA, &INFOO);
   }
 
+/*
   void dgeev_wrapper (SEXP JOBVL, SEXP JOBVR, SEXP N, SEXP A, SEXP LDA, SEXP WR,
 	      SEXP WI, SEXP VL, SEXP LDVL, SEXP VR, SEXP LDVR, SEXP WORK,
 	      SEXP LWORK, SEXP INFO, SEXP A_isBM, SEXP WR_isBM, SEXP WI_isBM,
