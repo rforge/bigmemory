960f70dbf7f9c82cebe8385948b7a4fe1510c062
jayemerson <jayemerson@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-07 19:33:27 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bigmemory@322 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bigvideo/video.R
+++
@@ -1,9 +0,0 @@
-# b = video()
-#x = cbind(as.vector(row(b)), as.vector(col(b)), as.vector(b))
-
-video = function() {
-  x = new('big.matrix',address=.Call("GrabVideo", as.integer(10)))
-  b = matrix(x[,1], 640, 480 )
-  y = cbind(as.vector(row(b)), as.vector(col(b)), as.vector(b))
-  return(y)
-}
