5054c472b96e8315e1eac403ce2d4cee90f77ad2
blewis <blewis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-09 23:41:50 +0000

    Compiles but not passing check yet
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bigmemory@347 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bigalgebra/src/ptr_util.hpp
+++ pkg/bigalgebra/src/ptr_util.hpp
@@ -30,28 +30,4 @@ double* make_double_ptr( SEXP matrix, SEXP isBigMatrix  )
   return(NUMERIC_DATA(matrix));
 };
 
-int* make_int_ptr( SEXP matrix, SEXP isBigMatrix )
-{
-  if (LOGICAL_VALUE(isBigMatrix) == (Rboolean)TRUE)
-  {
-    SEXP address = GET_SLOT(matrix, install("address"));
-    BigMatrix *pbm = reinterpret_cast<BigMatrix*>(R_ExternalPtrAddr(address));
-    if (!pbm)
-    {
-      return(NULL);
-    }
-// Functionality to support big.sub.matrices
-//    if (pbm->row_offset() > 0 && pbm->num_columns() > 1)
-//    {
-//      std::string errMsg = string("big.sub.matrix objects cannoth have row ") +
-//        string("offset greater than zero and number of columns greater than 1");
-//      Rf_error(errMsg.c_str());
-//      return(NULL);
-//    }
-    return(reinterpret_cast<int*>(pbm->matrix()) 
-      + pbm->col_offset()*pbm->nrow() + pbm->row_offset());
-  }
-  return(INTEGER_DATA(matrix));
-};
-
 #endif //BIGALGEBRA_PTR_UTIL
