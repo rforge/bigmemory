3b6b103b9d1ae2c12b8772b48eebaf82268ae8f3
kaneplusplus <kaneplusplus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-13 14:18:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bigmemory@428 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bigmemory/inst/include/bigmemory/isna.hpp
+++ pkg/bigmemory/inst/include/bigmemory/isna.hpp
@@ -11,15 +11,15 @@
 inline bool isna( const char val ) {return NA_CHAR == val;}
 inline bool isna( const short val ) {return NA_SHORT == val;}
 inline bool isna( const int val ) {return NA_INTEGER == val;}
-inline bool isna( const double val ) {return isnan(val);}
+inline bool isna( double val ) {return isnan(val);}
 
 inline bool neginf( const char val ) {return false;}
 inline bool neginf( const short val ) {return false;}
 inline bool neginf( const int val ) {return false;}
 #ifdef _MSC_VER
-inline bool isinf( const double val ) {return !_finite(val);}
+inline bool isinf( double val ) {return !_finite(val);}
 #endif
-inline bool neginf( const double val ) {return isinf(val) && val < 0;}
+inline bool neginf( double val ) {return isinf(val) && val < 0;}
 
 inline bool posinf( const char val );
 inline bool posinf( const short val );
